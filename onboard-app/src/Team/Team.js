d3f8b1afa59e0fd5c3b0a6464686802a2e930456
Antonio Caeiro <a.caeiro@runtime-revolution.com>
2020-09-16 11:19:47 +0100

    project restructure

--- onboard-app/src/Team/Team.js
+++ onboard-app/src/Team/Team.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Team.scss';
 
+class Person extends React.Component {
+  render() {
+    return (
+      /* section Intro */
+      <div className="tableElement">
+        <blockquote>
+        	<p><
+        </blockquote>
+        <a href="#" className={this.props.icon}></a>       
+        <h3>{this.props.title}</h3>
+        <p>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
 class Team extends React.Component {
   render() {
     return (
